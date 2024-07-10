02aa04ecb08f500597c66fa7dec104b9b07a660d
Pedro Lucas Kawano <pedro.lucas2107@outlook.com>
2024-07-10 17:36:54 -0300

    first commit

---
+++ Configuration.cs
@@ -0,0 +1,8 @@
+namespace Blog;
+
+public static class Configuration
+{
+    public static string JwtKey = "YzEzYTM2NzAtMGEyNC00MDdiLThkNTctNmZjYmJmMzg4MTBk";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+}
---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,23 @@
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("CONNECTION STRING");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}
